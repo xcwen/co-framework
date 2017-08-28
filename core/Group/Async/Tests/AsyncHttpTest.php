14609164638bf2a00ea0bc1839fe66e0209149e9
fucongcong <fucongcong@clothesmake.com>
2017-08-28 15:43:54 +0800

    rm test

--- core/Group/Async/Tests/AsyncHttpTest.php
+++ core/Group/Async/Tests/AsyncHttpTest.php
@@ -9,17 +9,17 @@ class AsyncHttpTest extends Test
 {
     public function unitget()
     {   
-        $http = new AsyncHttp('127.0.0.1', 80);
-        $http->setHost('groupco.com');
-        $res = (yield $http->get('/'));
+        // $http = new AsyncHttp('127.0.0.1', 80);
+        // $http->setHost('groupco.com');
+        // $res = (yield $http->get('/'));
         // $this->assertEquals('200', $res->statusCode);
         // $this->assertEquals('hello world!', $res->body);
     }
 
     public function unitpost()
     {   
-        $http = new AsyncHttp('127.0.0.1', 443, true);
-        $http->setHost('groupco.com');
-        $res = (yield $http->post('/test', ['postId' => 52]));
+        // $http = new AsyncHttp('127.0.0.1', 443, true);
+        // $http->setHost('groupco.com');
+        // $res = (yield $http->post('/test', ['postId' => 52]));
     }
 }
