73880283c3eeed6069e800f2ed8724e0b4b02c58
fucongcong <fucongcong@clothesmake.com>
2017-09-13 14:09:34 +0800

    fix the route file

--- core/Group/App/App.php
+++ core/Group/App/App.php
@@ -267,7 +267,7 @@ class App
 
         $routings = [];
         foreach ($sources as $source) {
-            $routing = include_once __ROOT__."src/{$source}/routing.php";
+            $routing = require_once __ROOT__."src/{$source}/routing.php";
             if ($routing) {
                 $routings = array_merge($routings, $routing);
             }
